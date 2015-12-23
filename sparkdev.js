bc980fdc0a29bd0943744cbad14cf3ab1ab03c15
drkchiloll <samwomack_@me.com>
2015-12-23 11:59:38 -0600

    implemented method to retrieve messages from a static room and format the messages into a single source/file

--- sparkdev.js
+++ sparkdev.js
@@ -1,27 +1,37 @@
 var moment = require('moment'),
     Promise = require('bluebird'),
-    sparkFactory = require('csco-spark');
+    sparkFactory = require('csco-spark'),
+    config = require('./config') || 'token';
 
 var spark = sparkFactory({
   uri: 'https://api.ciscospark.com/v1',
-  token: 'YzVkY2JmMTQtMjhlZC00NWQyLWE2NDgtNjk4NDU4Zjc1M2E2NDllNjQzNjctZDhk'
+  token: config.token || 'token'
 });
 var roomId =
   'Y2lzY29zcGFyazovL3VzL1JPT00vZWRiMTAxNjAtZWViMS0xMWU0LWJlNmQtZmY4ZTgwOTRiOGZj';
 spark.getMessages({
   roomId: roomId
 }).then(function(data) {
-  var msges;
+  var msges = '';
   //* 5/1/2015 8:30 PM
   var dateFormat = 'l LT';
   var messages = JSON.parse(data).items;
-  msges = messages.reduce(function(str, message) {
-    str += message.personEmail + ' ' +
-      moment(new Date(message.created)).format(dateFormat)+'\n' +
-      message.text+'\n\n';
-    return str;
-  }, '');
-
-
-  console.log(msges);
+  return Promise.reduce(messages, function(str, message) {
+    var msgCreated = new Date(message.created);
+    if(!message.personEmail) {
+      var personId = message.personId;
+      return spark.getPerson({personId: personId}).then(function(res) {
+        var person = JSON.parse(res);
+        str += person.emails[0] + ' ' +
+          moment(msgCreated).format(dateFormat) + '\n' + message.text + '\n\n';
+        return str;
+      });
+    } else {
+      str += message.personEmail + ' ' +
+        moment(msgCreated).format(dateFormat) + '\n' + message.text + '\n\n';
+      return str;
+    }
+  }, msges).then(function(res) {
+    console.log(res);
+  })
 })
