94cd7402818ca9a654fdb21e8445a4b28b13c624
drkchiloll <samwomack_@me.com>
2015-12-27 09:21:32 -0600

    wrote fetch process to get Access Token from React component

--- app/components/AuthAxxToken.js
+++ app/components/AuthAxxToken.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 export default class AuthAxxToken extends React.Component {
+  componentWillMount() {
+    var code = this.props.location.query.code;
+    fetch('/axxtoken', {
+      method: 'POST',
+      credentials: 'same-origin',
+      body: JSON.stringify({code: code})
+    }).then(function(resp) {
+      return resp.json()
+    }).then(function(data) {
+      console.log(data);
+    })
+  }
   render() {
     return (
       <div>Grant Axxs</div>
