7343620582937feda4ac038de6cdd35e8ae0ee27
drkchiloll <samwomack_@me.com>
2015-12-30 13:42:17 -0600

    render rooms in a Select Element

--- app/components/AuthAxxToken.js
+++ app/components/AuthAxxToken.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default class AuthAxxToken extends React.Component {
+  getInitialState() {
+    return {
+      rooms: null
+    };
+  }
   componentWillMount() {
     var code = this.props.location.query.code;
     fetch(`/axxtoken/${code}`, {
@@ -9,11 +14,35 @@ export default class AuthAxxToken extends React.Component {
       return resp.json()
     }).then(function(data) {
       console.log(data);
+      var token = data.access_token;
+      fetch(`/sparkrooms/${token}`, {
+        credentials: 'same-origin'
+      }).then(function(res) {
+        return res.json();
+      }).then(function(rooms) {
+        this.setState(rooms);
+      }.bind(this))
     })
   }
   render() {
+    var rooms = this.state.rooms;
     return (
-      <div>Grant Axxs</div>
+      {rooms ? this._renderRooms(rooms) :
+        <div> Waiting for Rooms To Load </div>
+      }
+    );
+  }
+  _renderRooms(rooms) {
+    return (
+      <div>
+        <div> Select a Room </div>
+        <select className='form-control'>
+          {return rooms.map((room) => {
+            return (
+              <option key={room.id}>{room.title}</option>
+            );
+          })}
+        </select>
     );
   }
 }
