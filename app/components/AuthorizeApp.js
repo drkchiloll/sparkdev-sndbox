a89f1a295298498e5ac7666e3539ff263998a3a4
drkchiloll <samwomack_@me.com>
2016-01-03 08:14:56 -0600

    added explaination of the application and resized the Auth App button

--- app/components/AuthorizeApp.js
+++ app/components/AuthorizeApp.js
@@ -10,11 +10,26 @@ export default class AuthorizeApp extends React.Component {
   }
   render() {
     return (
-      <button
-        className='btn btn-lg btn-success'
-        onClick={this.authenticate}>
-        Authorize App
-      </button>
+      <div>
+        <p>
+          This application pulls the rooms you have memberships in and
+          downloads the files that have been uploaded into those rooms on
+          your behalf.
+        </p>
+        <p>
+          If you would like this application to perform this task on your
+          behalf please click the <strong>Authorize App</strong> button.
+        </p>
+        <button
+          style={{
+            width: '200px',
+            length: '200px'
+          }}
+          className='btn btn-lg btn-success'
+          onClick={this.authenticate}>
+          Authorize App
+        </button>
+      </div>
     );
   }
 }
